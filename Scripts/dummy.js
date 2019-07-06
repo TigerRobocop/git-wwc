3ec70b158dccc1187c30f163ee1686c1eab7c7bc
alinecbsr <alinecbsr@gmail.com>
2019-07-06 15:01:52 -0300

    My first script

--- Scripts/dummy.js
+++ Scripts/dummy.js
@@ -1,5 +1,5 @@
-// LANGUAGE: Python
-// AUTHOR: Liv Souza
-// GITHUB: https://github.com/TigerRobocop
+// LANGUAGE: JavaScript
+// AUTHOR: Aline Rosa
+// GITHUB: https://github.com/alinecbsr
 
-console.log('Hello World!');
+console.log('Hello World! Women Who Code Recife _ workshop GitHub awesome!');
